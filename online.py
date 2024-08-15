d59645fdef247ff38e38e670cdcd20ab4df74322
MamawliV2 <168989230+MamawliV2@users.noreply.github.com>
2024-08-15 22:51:42 +0330

    Update online.py

--- online.py
+++ online.py
@@ -5,6 +5,7 @@ import asyncio
 api_id = input("Please enter your API ID: ")
 api_hash = input("Please enter your API Hash: ")
 phone_number = input("Please enter your phone number: ")
+channel_username = input("Please enter the channel username (e.g., @channelusername): ")
 
 # ایجاد یک کلاینت تلگرام
 client = TelegramClient('session_name', api_id, api_hash)
@@ -14,9 +15,10 @@ async def main():
     await client.start(phone=phone_number)
     print("Client Created and Online")
 
-    # نگه داشتن کلاینت آنلاین
+    # نگه داشتن کلاینت آنلاین با ارسال پیام به کانال مشخص شده
     while True:
-        await asyncio.sleep(60)  # هر 60 ثانیه یکبار منتظر می‌ماند تا آنلاین بماند
+        await client.send_message(channel_username, 'Keeping the channel active')
+        await asyncio.sleep(5)  # هر 5 ثانیه یکبار پیام ارسال می‌کند
 
 # اجرای برنامه
 client.loop.run_until_complete(main())
